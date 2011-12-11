0fb4a34ff0e6020d5af1edf3a35e6efc417b6a7a
Jonathan Wolter <jaw@jawspeak.com>
2011-12-10 18:42:46 -0800

    working integration test

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,33 +1,38 @@
 require 'rspec'
 require 'render_docx_template'
 require 'template_processor_spec'
+require 'fileutils'
 
 describe "integration test" do
   let(:data) { DocxTemplater::TestData::DATA }
-  let(:input_file) { '../ExampleTemplate.docx' }
-  let(:output_file) { 'spec/tmp/IntegrationTestOutput.docx' }
-  before { File.delete(output_file) }
+  let(:input_file) { "ExampleTemplate.docx" }
+  let(:output_dir) { "spec/tmp" }
+  let(:output_file) { "#{output_dir}/IntegrationTestOutput.docx" }
+  before do 
+    FileUtils.rm_rf(output_dir) if File.exists?(output_dir)
+    Dir.mkdir(output_dir)
+  end
 
   context "should process in incoming docx" do
     it "generates a valid zip file (.docx)" do
-        DocxCreator.new(input_file, data).generate_docx_file(output_file)
+      DocxTemplater::DocxCreator.new(input_file, data).generate_docx_file(output_file)
 
-        archive = Zip::Archive.open(output_file)
-        archive.close
+      archive = Zip::Archive.open(output_file)
+      archive.close
 
-        puts "************************************"
-        puts "   >>> Only will work on mac <<<"
-        puts "NOW attempting to open created file in Word."
-        cmd = "open #{output_file}"
-        puts "  will run '#{cmd}'"
-        puts "************************************"
+      puts "\n************************************"
+      puts "   >>> Only will work on mac <<<"
+      puts "NOW attempting to open created file in Word."
+      cmd = "open #{output_file}"
+      puts "  will run '#{cmd}'"
+      puts "************************************"
 
-        system cmd
+      system cmd
     end
 
     it "generates a file with the same contents as the input docx" do
       input_entries = Zip::Archive.open(input_file) { |z| z.map(&:name) }
-      DocxCreator.new(input_file, data).generate_docx_file(output_file)
+      DocxTemplater::DocxCreator.new(input_file, data).generate_docx_file(output_file)
       output_entries = Zip::Archive.open(output_file) { |z| z.map(&:name) }
 
       input_entries.should == output_entries
