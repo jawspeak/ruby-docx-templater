18978435368608521670cfbaa7b7653da8187872
Tamir Duberstein <tamird@squareup.com>
2014-04-04 08:29:31 -0700

    force new rspec syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,11 @@ require 'fileutils'
 require 'docx_templater'
 
 SPEC_BASE_PATH = Pathname.new(File.expand_path(File.dirname(__FILE__)))
+
+RSpec.configure do |config|
+  [:expect_with, :mock_with].each do |method|
+    config.send(method, :rspec) do |c|
+      c.syntax = :expect
+    end
+  end
+end
